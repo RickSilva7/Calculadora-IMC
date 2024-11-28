1f8346803ce53db688704a16b0cde1aaecf90bd0
Henrique <rick-silva8@outlook.com>
2024-11-28 17:00:57 +0100

    Calculando o IMC

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -16,8 +16,12 @@ class MainActivity : AppCompatActivity() {
 
         btnCalcular.setOnClickListener {
 
-            val peso = edtPeso.text
-            println("Henrique" + peso)
+            val peso:Float = edtPeso.text.toString().toFloat()
+            val altura:Float = edtAltura.text.toString().toFloat()
+
+            val alturaQ2 = altura * altura
+            val resultado = peso / alturaQ2
+            println("Henrique" + resultado)
         }
 
     }
